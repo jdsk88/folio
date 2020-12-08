9315dca3a341821e9021806bdd347e9bf772e026
jdsk88 <maciejjakobszy@me.com>
2020-12-08 13:30:11 +0100

    layout fix v.1.0 & pwa

---
+++ src/components/layout.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+console.log('Layout componentd loaded');
+
+const Layout = () => {
+    return (<>
+        <div className="page-layout"></div>
+    </>);
+
+}
+export default Layout
